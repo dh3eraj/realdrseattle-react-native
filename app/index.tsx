fb409138dac724f96b8378cd7eb400f45e3b3ff2
dh3eraj <dheerajgupta200002@gmail.com>
2025-03-14 19:27:04 +0530

    UI enhanced

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import RootStack from "../navigation/RootStack";
 import { Provider } from "react-redux";
 import store from "../redux/Store";
-import UsersScreen from "../screens/UsersScreen";
 export default function index() {
   return (
     <Provider store={store}>
--- navigation/RootStack.tsx
+++ navigation/RootStack.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
 
-import UsersScreen from '../screens/UsersScreen';
+import UsersScreen from '../screens/PostsScreen';
 
 export type RootStackParamList = {
   Home: undefined;
